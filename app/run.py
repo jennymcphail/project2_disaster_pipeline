dd7bd5c32cd82e3564aca4686dae13dbf08f04e9
jennymcphail <92020226+jennymcphail@users.noreply.github.com>
2021-12-15 15:01:44 +0000

    Update run.py
    Adding import statement for plotly Pie graph object

--- app/run.py
+++ app/run.py
@@ -8,6 +8,7 @@ from nltk.tokenize import word_tokenize
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
+from plotly.graph_objs import Pie
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
 
